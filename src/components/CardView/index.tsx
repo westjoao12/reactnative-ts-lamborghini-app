501dc44edf732c9f99247eb61f954de514991b49
westjoao12 <westjoao12@gmail.com>
2025-09-09 22:23:02 +0100

    feat: Visualizador de carros

--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -5,18 +5,38 @@ import { View, Text, Button, Image } from "react-native";
 const Logo = require('../../../assets/logo.png')
 import { styles } from "./style";
 import Divider from "../Divider";
+import { CAR_ASSETS_BASE_URL } from "../../constants/car";
 
 export default function CardView() {
 
     const renderLogoBox = () => (
         <View style={styles.logoContainer}>
-        <Image style={styles.imageLogo} source={Logo} />
+            <Image style={styles.imageLogo} source={Logo} />
         </View>
+    );
+
+    const renderCarDetails = () => (
+        <View style={{ alignItems: "center" }}>
+            <Text style={styles.carBrand}>Lamborghini</Text>
+            <Text style={styles.carName}>MODEL</Text>
+        </View>
+    )
+
+    const renderCarImage = () => (
+        <Image 
+            style={styles.image}
+            source={{uri: `${CAR_ASSETS_BASE_URL}1.png`}}
+        />
     );
 
     return (
         <View style={styles.imageContainer}>
             {renderLogoBox()}
+
+            <Divider />
+
+            {renderCarDetails()}
+            {renderCarImage()}
 
             <Divider />
         </View>
---
+++ src/constants/car.ts
@@ -0,0 +1,1 @@
+export const CAR_ASSETS_BASE_URL = "https://digitalinnovationone.github.io/fake-data-api-lamborghini/assets/"
